77fc8993c4bb19e622167252b7a1a90e7c868c31
= <ardayildirim05@icloud.com>
2023-07-22 12:34:30 +0300

    Disable automatic word refetching

--- src/hooks/useWords.ts
+++ src/hooks/useWords.ts
@@ -1,11 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import wordService from "../services/wordService";
+import ms from "ms";
 
 const useWords = () =>
   useQuery({
     queryKey: ["words"],
     queryFn: () => wordService.getAll(),
     refetchOnWindowFocus: false,
+    staleTime: ms("1d"),
   });
 
 export default useWords;
